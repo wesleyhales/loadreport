dbb572f4e43bd11f81e276892a4d3298f24ae3e8
Jerome Covington <jeromecovington@gmail.com>
2013-07-01 22:13:01 -0400

    Added increment to console.log output.

--- schedule-loadreport.js
+++ schedule-loadreport.js
@@ -5,18 +5,18 @@ var increment = 0;
 function runLoadreport(){
     exec('phantomjs loadreport.js http://www.people.com performance csv', function (error, stdout, stderr) {
         exec('mv reports/loadreport.csv reports/loadreport-' + increment + '.csv', function(error, stdout, stderr){
-        	console.log('CSV done.');
+        	console.log('CSV ' + increment + ' done.');
         });
     });
     exec('phantomjs loadreport.js http://www.people.com performancecache json', function (error, stdout, stderr) {
         exec('mv reports/loadreport.json reports/loadreport-' + increment + '.json', function(error, stdout, stderr){
-        	console.log('JSON done.');
+        	console.log('JSON ' + increment + ' done.');
         });
     });
     exec('phantomjs loadreport.js http://www.people.com filmstrip', function (error, stdout, stderr) {
         exec('mkdir filmstrip/filmstrip-' + increment, function(error, stdout, stderr){
         	exec('mv filmstrip/screenshot*.png filmstrip/filmstrip-' + increment + '/', function(error, stdout, stderr){
-        		console.log('Filmstrip done.')
+        		console.log('Filmstrip ' + increment + ' done.')
         	});
         });
     });
