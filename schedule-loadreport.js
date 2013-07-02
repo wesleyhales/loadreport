b8cccbc1d97303781481676afe0dee4ed93fa726
Jerome Covington <jeromecovington@gmail.com>
2013-07-01 21:51:54 -0400

    Correct path to reports and move increment to runLoadreport function.

--- schedule-loadreport.js
+++ schedule-loadreport.js
@@ -4,12 +4,12 @@ var increment = 0;
 
 function runLoadreport(){
     exec('phantomjs loadreport.js http://www.people.com performance csv', function (error, stdout, stderr) {
-        exec('mv report/loadreport.csv report/loadreport-' + increment + '.csv', function(error, stdout, stderr){
+        exec('mv reports/loadreport.csv reports/loadreport-' + increment + '.csv', function(error, stdout, stderr){
         	console.log('CSV done.');
         });
     });
     exec('phantomjs loadreport.js http://www.people.com performancecache json', function (error, stdout, stderr) {
-        exec('mv report/loadreport.json report/loadreport-' + increment + '.json', function(error, stdout, stderr){
+        exec('mv reports/loadreport.json reports/loadreport-' + increment + '.json', function(error, stdout, stderr){
         	console.log('JSON done.');
         });
     });
@@ -20,7 +20,8 @@ function runLoadreport(){
         	});
         });
     });
-    makeTimeout();    
+    makeTimeout();
+    increment++;    
 }
 
 function makeTimeout(){
@@ -32,9 +33,7 @@ function makeTimeout(){
         runLoadreport();
     } else {
         setTimeout(runLoadreport, (60*(60-min)+(60-sec))*1000);
-    }
-        
-    increment++;
+    }            
 }
 
 makeTimeout();
